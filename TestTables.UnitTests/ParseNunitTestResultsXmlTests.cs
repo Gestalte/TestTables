615f7adafb8d986c73cb70fc7b5dc41e8cb3d264
Gestalte <philipbenade@gmail.com>
2021-01-30 15:50:51 +0200

    Created project and test project

---
+++ TestTables.UnitTests/ParseNunitTestResultsXmlTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestTables.UnitTests
+{
+    [TestFixture]
+    public class ParseNunitTestResultsXmlTests
+    {
+
+    }
+}
