e886e06664fe8b7683735ed0dce856b911e5995a
Gestalte <philipbenade@gmail.com>
2021-02-02 23:24:06 +0200

    Figured out how to unit test in F#

---
+++ NUnitTestResultParser.UnitTests/XmlTests.fs
@@ -0,0 +1,24 @@
+﻿module XmlTests
+
+open NUnit.Framework
+open Xml
+open FSharp.Data
+
+type Json = JsonProvider<"config.json", EmbeddedResource="lib, lib.embedded.config.json">
+
+type t = Json.Root
+
+let load (path: string): t =
+    Json.Load(path)
+
+let testPath (config: t): string =
+    config.TestPath
+
+[<TestFixture>]
+type TestClass() =
+
+    [<Test>]
+    member this.LoadXml() =
+        let filePath = testPath(load("config.json"))
+        let result = Xml.LoadXml(filePath)
+        Assert.IsNotNull(result)
---
+++ NUnitTestResultParser/Xml.fs
@@ -0,0 +1,6 @@
+﻿module Xml
+
+open System.Xml.Linq
+
+    let LoadXml (filePath:string) =
+        XElement.Load(filePath)
